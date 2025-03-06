d458e5cef05a85e367fa065c6c2a556abaa1a7ce
FadnisW <fadnisw@gmail.com>
2025-03-07 00:25:30 +0530

    added navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return(
         <main className="fopnt-work-sans">
+            <Navbar />
 
             {children}
         </main>
