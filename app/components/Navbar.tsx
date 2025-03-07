612f3be4a7a87f6de4570e5e6afdd0b8513534a6
FadnisW <fadnisw@gmail.com>
2025-03-07 18:13:16 +0530

    trying to style using tailwindcss

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-md font-work-sans">
       <nav className='flex justify-between items-center'>
         <Link href='/'>
-        <Image src="/logo.png" alt="Logo" width={40} height={7} />
+        <Image src="/logo.png" alt="Logo" width={90} height={50} />
         </Link>
         <div className='flex items-center gap-5 text-black'>
             {session && session?.user ? (
