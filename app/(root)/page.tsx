5839bad36d2e152201e709e86de67a2d95e267b7
FadnisW <fadnisw@gmail.com>
2025-03-07 20:53:40 +0530

    added a search input option in the hero section and added shadcn button at the same time made some changes in the path

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: { 
+  searchParams: Promise<{ query?: string }>
+ }) {
+  const query = (await searchParams).query;
   return (
     <>
-    <section className="pink_container">
-          <h1 className="heading">Pitch, Vote, and launch your startup idea</h1>    
-          <p className="sub-heading !max-w-3xl"> Submit your Ideas, Vote on Pitches, Get Noticed by the Community</p>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch, Vote, and <br /> launch your startup idea
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          {" "}
+          Submit your Ideas, Vote on Pitches, Get Noticed by the Community
+        </p>
+        <SearchForm query = {query} />
+      </section>
     </>
   );
 }
